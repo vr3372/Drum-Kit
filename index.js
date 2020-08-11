c119a1d6adcde21b76d7d5d2188f043d4631470e
Rajesh V R <48948666+vr3372@users.noreply.github.com>
2020-08-11 23:37:59 +0530

    rrrr

--- index.js
+++
@@ -1,7 +0,0 @@
-let totalButtons = document.querySelectorAll(".drum");
-for (let i = 0; i < totalButtons.length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    let audio = new Audio("sounds/tom-3.mp3");
-    audio.play();
-  });
-}
